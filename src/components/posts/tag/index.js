08e57ad42802d9885a706e0f651e4a64e97b99d0
Hien Nguyen <biro@Hiens-MacBook-Air.local>
2019-12-17 23:29:48 -0800

    Added Search Bar

--- src/components/posts/tag/index.js
+++ src/components/posts/tag/index.js
@@ -16,11 +16,22 @@ import './tag.css';
 
 class Tag extends Component{
 
+    constructor(props){
+        super(props);
+        this.state = {
+            active: 0
+        }
+    }
+
     /**
      * Import the tagID 
      * @param {string} tagID 
      */
     getPostsByTag(tagID){
+
+        // set active tag 
+        this.setState({ active: tagID });
+
         // sending tagID, to make sure the 
         // next 10 posts are in the same tag 
         this.props.getPostsByTag(tagID);
@@ -32,13 +43,15 @@ class Tag extends Component{
     }
 
     render(){
+        var {active} = this.state;
+
         return(
                 <div id="posts__tag">
-                    <div className="posts__tags--circle" onClick={()=> this.getPostsByTag('0')}><img src={IconNewsPaper} alt='all'/></div>
-                    <div className="posts__tags--circle" onClick={()=> this.getPostsByTag('1')}><img src={IconNetwork} alt='org'/></div>
-                    <div className="posts__tags--circle" onClick={()=> this.getPostsByTag('2')}><img src={IconFirework} alt='event'/></div>
-                    <div className="posts__tags--circle" onClick={()=> this.getPostsByTag('3')}><img src={IconTent} alt='place'/></div>
-                    <div className="posts__tags--circle" onClick={()=> this.getPostsByTag('4')}><img src={IconSocial} alt='hot deal'/></div>
+                    <div className={`posts__tags--circle`} id={active ==='0' ? 'posts__tags--active':''} onClick={()=> this.getPostsByTag('0')}><img src={IconNewsPaper} alt='all'/></div>
+                    <div className={`posts__tags--circle`} id={active ==='1' ? 'posts__tags--active':''} onClick={()=> this.getPostsByTag('1')}><img src={IconNetwork} alt='org'/></div>
+                    <div className={`posts__tags--circle`} id={active ==='2' ? 'posts__tags--active':''} onClick={()=> this.getPostsByTag('2')}><img src={IconFirework} alt='event'/></div>
+                    <div className={`posts__tags--circle`} id={active ==='3' ? 'posts__tags--active':''} onClick={()=> this.getPostsByTag('3')}><img src={IconTent} alt='place'/></div>
+                    <div className={`posts__tags--circle`} id={active ==='4' ? 'posts__tags--active':''} onClick={()=> this.getPostsByTag('4')}><img src={IconSocial} alt='hot deal'/></div>
                 </div>
         )
     }
